902d6e60f3d57b9f848b035541b32502e2176d9a
btwang05 <btwang05@gmail.com>
2017-04-06 02:40:47 +0000

    Added time log to checkpoint_send.sh

--- p2pfs/p2p/checkpoint_send.sh
+++ p2pfs/p2p/checkpoint_send.sh
@@ -12,7 +12,8 @@ DEST_IP=$2
 DEST_DIR="/home/bw2387/test_scripts"
 USERNAME="bw2387"
 P2P_DIR="/home/bw2387/vmtorrent/p2pfs/p2p"
-
+KEY="/home/bw2387/.ssh/vmtorrent_key"
+TRIAL=1
 trap ctrl_c INT
 
 # on ctrc_c, kill test_p2p processes
@@ -21,8 +22,12 @@ function ctrl_c() {
     exit 0
 }
 
-if [ "$#" -eq 3 ]; then
+if [ "$#" -ge 3 ]; then
     INTERVAL=$3
+fi
+
+if [ "$#" -ge 4 ]; then
+    TRIAL=$4
 fi
 
 while true; do
@@ -51,15 +56,22 @@ while true; do
 
     if [ -d "$directoryname$movefolder" ]; then
 
+        echo "TRIAL $TRIAL" >> logtime.txt
+        START=$(date +'%Y-%m-%d %H:%M:%S.%3N')
+        echo "compress start: $START" >> logtime.txt
         tar -cvzf $directoryname$movefolder.tar.gz $directoryname$movefolder
+        END=$(date +'%Y-%m-%d %H:%M:%S.%3N')
+        echo "compress end: $END" >> logtime.txt
         rm -rf $directoryname$movefolder
 
         # kill old test_p2p
         pkill -9 test_p2p
 
+        echo "make torrent start: $(date +'%Y-%m-%d %H:%M:%S.%3N')" >> logtime.txt
         $P2P_DIR/make_torrent -t $directoryname.torrent -f $directoryname$movefolder.tar.gz -s 256
-        scp $directoryname.torrent $USERNAME@$DEST_IP:$DEST_DIR
-
+        scp -i $KEY $directoryname.torrent $USERNAME@$DEST_IP:$DEST_DIR
+        
+        echo "test p2p start: $(date +'%Y-%m-%d %H:%M:%S.%N')" >> logtime.txt
         $P2P_DIR/test_p2p -t $directoryname.torrent -l . &
     fi
         
