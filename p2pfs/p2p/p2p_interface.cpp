4698c47dd6ab75e049efd069d580364256880d69
kvm2116 <kvm2116@columbia.edu>
2018-02-07 09:33:36 -0500

    Update p2p_interface.cpp
    Commented leveldb add hashes

--- p2pfs/p2p/p2p_interface.cpp
+++ p2pfs/p2p/p2p_interface.cpp
@@ -19,7 +19,7 @@
 
 #include "p2p_interface.hpp"
 
-#include "leveldb/db.h"
+//#include "leveldb/db.h"
 
 using namespace libtorrent;
 bool compact_mode = false;
@@ -207,7 +207,7 @@ int p2p_interface::start_torrent(const char *torrent_file, const char *dir, int 
 int p2p_interface::start_torrent(const char *torrent_file, const char *dir, int demand, int upload_limit, int download_limit) {
     start_torrent_(torrent_file, dir, -1, -1, demand, upload_limit, download_limit);
 }
-
+/*
 int add_hashes_db() {
 	leveldb::DB* db;
 	leveldb::Options options;
@@ -229,7 +229,7 @@ int add_hashes_db() {
 }
 
 
-
+*/
 
 int p2p_interface::start_torrent_(const char *torrent_file, const char *dir, int min_port, int max_port, int demand, int upload_limit, int download_limit) {
     if (min_port == -1) min_port = DEFAULT_MIN_PORT;
@@ -238,7 +238,7 @@ int p2p_interface::start_torrent_(const char *torrent_file, const char *dir, int
     P2P_OUT << "start" 
 	    << P2P_ENDL;
    
-	add_hashes_db(); 
+//	add_hashes_db(); 
     int nice = getpriority (PRIO_PROCESS, 0);
     P2P_OUT << "nice=" << nice
 	    << P2P_ENDL;
